37587cab547676403e431d3d0a36dcbbae96ba7d
zxwnstn <zxwnstn@naver.com>
2020-01-31 17:18:40 +0900

    first commit
    second commit

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+workspace "testGit"
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+	platforms
+	{
+		"x86",
+	}
+	characterset ("MBCS")
+	startproject "testGit"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "testGit"
+	location "testGit"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+	}
+	includedirs
+	{
+		"%{prj.name}/src",
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		symbols "On"
+	
+	filter "configurations:Release"
+		optimize "On"
