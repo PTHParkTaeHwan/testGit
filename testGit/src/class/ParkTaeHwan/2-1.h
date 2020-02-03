e55ae45eac0ecb15d755a84fad94f970d1af165f
PTHParkTaeHwan <pth0810@naver.com>
2020-02-03 17:56:24 +0900

    클래스 초기화 함수 추가

--- testGit/src/class/ParkTaeHwan/2-1.h
+++ testGit/src/class/ParkTaeHwan/2-1.h
@@ -10,6 +10,8 @@ public:
 private:
 	std::shared_ptr<int> Ptr2_1;
 	std::vector<std::shared_ptr<int>> _vPtr;
+
+	void ptrInit();
 };
 
 pth2_1::pth2_1()
@@ -17,6 +19,10 @@ pth2_1::pth2_1()
 }
 
 pth2_1::~pth2_1()
+{
+}
+
+inline void pth2_1::ptrInit()
 {
 }
 
