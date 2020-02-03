e5cf8ba8f957b5372cd8379e58ef6e38a82eaf1f
PTHParkTaeHwan <pth0810@naver.com>
2020-02-03 18:21:20 +0900

    스마트포인터실험중

--- testGit/src/class/ParkTaeHwan/2-1.h
+++ testGit/src/class/ParkTaeHwan/2-1.h
@@ -8,7 +8,7 @@ public:
 	~pth2_1();
 
 private:
-	std::shared_ptr<int> Ptr2_1;
+	std::shared_ptr<int> Ptr2_1(new int(10));
 	std::vector<std::shared_ptr<int>> _vPtr;
 
 	void ptrInit();
@@ -25,5 +25,7 @@ pth2_1::~pth2_1()
 inline void pth2_1::ptrInit()
 {
 	cout << "init" << endl;
+	string str = "가나다라";
+	Ptr2_1. = &str;
 }
 
