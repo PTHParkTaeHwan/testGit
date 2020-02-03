6a16be6d9e32dcc8af788a4be7bd88557bc53632
playdual <playdual@naver.com>
2020-02-03 17:54:31 +0900

    인트 1추가

--- testGit/src/class/SunwooKim/3-1.h
+++ testGit/src/class/SunwooKim/3-1.h
@@ -1,1 +1,23 @@
 #pragma once
+#include"class/type.h"
+
+class Sunwoo_1
+{
+public:
+	Sunwoo_1();
+	~Sunwoo_1();
+
+private:
+
+	std::shared_ptr<Item> ptr_1;
+	std::vector<std::shared_ptr<Item>> _vPtr_1;
+	int i;
+};
+
+Sunwoo_1::Sunwoo_1()
+{
+}
+
+Sunwoo_1::~Sunwoo_1()
+{
+}
